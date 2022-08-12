56e59b86035dfc478a07b461a8dcc58ac29ec8ab
kuku-kun <melon813@naver.com>
2022-08-12 20:30:27 +0900

    2주차 django 개발3

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Question(models.Model):
+    FullName = models.CharField(max_length=200)
+    Email = models.EmailField()
+    PhoneNumber = models.CharField(max_length=200)
+    Message = models.TextField()
+    pub_date = models.DateTimeField('date published')
